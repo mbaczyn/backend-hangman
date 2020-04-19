63c65619574cb3e51f589a4f15d00fb274a570fa
Michal <44243679+mbaczyn@users.noreply.github.com>
2020-04-19 17:19:58 +0200

    Spring initializr

---
+++ src/test/java/com/baczynski/hangman/HangmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baczynski.hangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
